502f518042628b40b6eba1c5287f504de921f6df
unknown <lestarisinurat123@gmail.com>
2020-05-08 09:48:03 +0700

    init

---
+++ app/src/main/java/com/loginkt/data/ui/main/activity/DetailPenginapanActivity.kt
@@ -0,0 +1,13 @@
+package com.loginkt.data.ui.main.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loginkt.R
+
+class DetailPenginapanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_penginapan)
+    }
+}
